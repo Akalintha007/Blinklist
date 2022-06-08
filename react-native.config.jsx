9ea4aab6aba6b242b3c8c23eb313db1a69478efd
akalintha007 <akalinthaofficial@gmail.com>
2022-06-08 21:29:55 +0530

    first commit

---
+++ react-native.config.jsx
@@ -0,0 +1,4 @@
+module.exports  = {
+    asets:'./cera-pro'
+
+}
---
+++ src/components/organisms/Header/index.test.tsx
@@ -0,0 +1,13 @@
+import Footer from '.';
+// import render from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
+
+
+test('Footer', () => {
+    render(
+        <Footer />
+    );
+  
+  
+});
+
